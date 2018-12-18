2a8ea4815a48af91c919a5a05e93d8808ab05764
student <student@imac-19.local>
2018-12-18 11:37:52 +0800

    写完

--- 实验十animation/ViewController.swift
+++ 实验十animation/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak weakSelf=self] (mytimer) in weakSelf?.myView.center.x += 20
+        })
     }
 
-
+    @IBAction func btnClicked(_ sender: Any) {
+        timer?.invalidate()
+        UIView.animate(withDuration: 4, delay: 1, options: [.curveLinear,.autoreverse], animations: {
+            self.myView.alpha = 0
+            self.myView.center.x = self.view.bounds.width
+            self.myView.backgroundColor = UIColor.purple
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+            self.myView.transform = CGAffineTransform.identity
+        }) { (finished) in
+            if finished{
+                self.myView.removeFromSuperview()
+        }
+        }
+    }
+    
 }
 
